00b71bbc3669301fb00137015980d542504605cb
Maria Keating <maria@keatingfamily.moe>
2018-11-27 00:09:04 +0800

    Fixed formatting in vector

--- src/vector.c
+++ src/vector.c
@@ -2,10 +2,8 @@
 
 #include <stdlib.h>
 
-void pushVector(vector *dest, void *value)
-{
-    if(dest->size + 1 <= dest->maxSize)
-    {
+void pushVector(vector *dest, void *value) {
+    if (dest->size + 1 <= dest->maxSize) {
         dest->elements[dest->size] = value;
         dest->size+=1;
     }
